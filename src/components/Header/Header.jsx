6b4a973c698f91b03e0c30c0e08121133ce7019f
founders <ser.razum2003@gmail.com>
2025-08-17 23:36:20 +0400

    Add files via upload

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,14 +6,14 @@ function Header() {
             <div className={"header-mobile"}>
                <div className={"header-navigator"}>
                    <button className={"burger__menu"}>
-                       <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="currentColor"
+                       <svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor"
                             className="bi bi-list" viewBox="0 0 16 16">
                            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                        </svg>
                    </button>
                    <Link className={"link__logo"} to="/">Okey</Link>
-                   <button className={"burger__menu"}>
-                       <svg xmlns="http://www.w3.org/2000/svg" width="25" fill="currentColor"
+                   <button className={"other__menu"}>
+                       <svg xmlns="http://www.w3.org/2000/svg" width="30" fill="currentColor"
                             className="bi bi-three-dots-vertical" viewBox="0 0 16 16">
                            <path d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0m0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0"/>
                        </svg>

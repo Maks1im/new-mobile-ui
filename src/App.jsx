f8e7e0d5f20142a4734549d5883bed7d70259ee3
founders <ser.razum2003@gmail.com>
2025-08-18 10:55:18 +0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+import {lazy, Suspense} from "react";
 import './styles/app.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import HomePage from "./pages/HomePage/HomePage.jsx";
+//imports components
+const HomePage =
+    lazy(() => import("./pages/HomePage/HomePage.jsx"));
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path={"/"} element={<HomePage />}/>
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route
+                        path="/" 
+                        element={<Suspense fallback={<h1>Loading...</h1>}><HomePage /></Suspense>}/>
+                </Routes>
+            </BrowserRouter>
+        </>
+    )
 }
 
-export default App
+export default App;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,11 @@
 import Header from "../../components/Header/Header.jsx";
+import MenuMobile from "../../components/MenuMobile/MenuMobile.jsx";
 
 function HomePage() {
     return (
         <>
             <Header />
-
+            <MenuMobile/>
         </>
     )
 }
